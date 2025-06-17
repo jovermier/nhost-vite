8336769d580f5f632350c091977e81193d8866b6
Jason Overmier <jason.r.overmier@gmail.com>
2025-06-17 12:15:05 -0500

    fix: nhost auth

--- src/utils/nhost.ts
+++ src/utils/nhost.ts
@@ -1,7 +1,7 @@
 import { NhostClient } from "@nhost/react";
 
 const nhost = new NhostClient({
-  subdomain: import.meta.env.VITE_NHOST_SUBDOMAIN || "localhost",
+  subdomain: import.meta.env.VITE_NHOST_SUBDOMAIN || "local",
   region: import.meta.env.VITE_NHOST_REGION,
 });
 
--- vite.config.mjs
+++ vite.config.mjs
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ["@nhost/react"],
-  },
   plugins: [tailwindcss(), react()],
 });
